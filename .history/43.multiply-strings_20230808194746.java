a697280acccc432b8110924b4b88bee93cc8514e
Jace9999 <jacezhu99@gmail.com>
2023-08-08 20:00:47 -0400

    8.8 rest day

---
+++ .history/43.multiply-strings_20230808194746.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=43 lang=java
+ *
+ * [43] Multiply Strings
+ */
+
+// @lc code=start
+class Solution {
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")){
+            return "0";
+        }
+        int n1 = 0;
+        int n2 = 0;
+        int s1 = 0;
+        int e1 = num1.length();
+    }
+}
+// @lc code=end
+
---
+++ 21.merge-two-sorted-lists.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode(0);
+        ListNode temp = res;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                res.next = list1;
+                list1 = list1.next;
+            }else{
+                res.next = list2;
+                list2 = list2.next;
+            }
+                res = res.next;
+        }
+        if(list1!=null){
+            res.next = list1;
+        }
+        if(list2!=null){
+            res.next = list2;
+        }
+        return temp.next;
+    }
+}
+// @lc code=end
+
